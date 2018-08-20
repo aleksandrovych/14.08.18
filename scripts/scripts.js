a889c0640f5d8557ecbd59c50955ef8824c3e4b3
Allar-Alexey Alexandrovich <allar5430@gmail.com>
2018-08-20 15:43:46 +0300

    add elements

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,3 +2,13 @@
 function headerItemClicked (button) {
     button.selected = !button.selected
 }
+
+function createList() {
+    var grid = document.forms["formgrid"]
+    var i = 0
+    for (; i < 10; i++) {
+        var item = document.querySelector("#templateitem")
+        grid.appendChild(item.content.cloneNode(true));
+    }
+
+}
